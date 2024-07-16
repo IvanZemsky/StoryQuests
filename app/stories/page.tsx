05f102d9e8fe7be60fc82b6bacc9962fe2d99eb3
IvanZemsky <i.ivanzemsky2005@gmail.com>
2024-07-16 03:10:00 +0300

    next config fix: page file extensions

--- app/stories/page.tsx
+++ app/stories/page.tsx
@@ -1,11 +1,2 @@
-import { Wrapper } from "@/src/shared"
-import { StoriesFilters } from "@/src/widgets/StoriesFilters"
-
-const page = () => {
-   return (
-      <Wrapper>
-         <StoriesFilters />
-      </Wrapper>
-   )
-}
-export default page
+import { Stories } from "@/src/pages/Stories"
+export default Stories
