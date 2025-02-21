130bf860feebe1bff54f61b885daa1160f6e4700
IvanZemsky <i.ivanzemsky2005@gmail.com>
2025-02-21 12:48:55 +0300

    fix: Home page initial nodes

--- src/pages_/Home/model/initialNodes.ts
+++ src/pages_/Home/model/initialNodes.ts
@@ -2,21 +2,21 @@ import { SceneNode } from "@/entities/Scene"
 
 export const initialNodes: SceneNode[] = [
    {
-      id: "scene_1",
+      id: "1",
       type: "startScene",
       position: { x: 320, y: 25 },
-      data: { title: "Create a scene", description: "", img: "", type: "default" },
+      data: { number: "1", title: "Create a scene", description: "", img: "", type: "default" },
    },
    {
-      id: "scene_2",
+      id: "2",
       type: "scene",
       position: { x: 320, y: 125 },
-      data: { title: "Add more scenes", description: "", img: "", type: "default" },
+      data: { number: "2", title: "Add more scenes", description: "", img: "", type: "default" },
    },
    {
-      id: "scene_3",
+      id: "3",
       type: "endScene",
       position: { x: 320, y: 225 },
-      data: { title: "Add endings", description: "", img: "", type: "default" },
+      data: { number: "3", title: "Add endings", description: "", img: "", type: "default" },
    },
 ]
